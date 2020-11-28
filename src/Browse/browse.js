a37a1fa370b3ffea79b9faee061ed6bd9af28efb
nevillejodie <jlneville92@gmail.com>
2020-11-28 10:06:40 +0000

    draft browse page done

--- src/Browse/browse.js
+++ src/Browse/browse.js
@@ -1,7 +1,12 @@
 import Card from '../Card/card'
 import Grid from "@material-ui/core/Grid";
 import React from 'react'
-import css from './browse.css'
+import one from '../images/one.jpg'
+import two from '../images/two.jpg'
+import three from '../images/three.jpg'
+import four from '../images/four.jpg'
+import five from '../images/five.jpg'
+import six from '../images/six.jpg'
 
 export default function Browse() {
    
@@ -9,24 +14,24 @@ export default function Browse() {
     return (<div styles={'margin-top: 30px'}>
         <Grid container spacing={2}>
           <Grid item xs>
-            <Card typography="1" description="description of item 1"></Card>
+            <Card img={one} typography="1" description="description of item 1"></Card>
           </Grid>
           <Grid item xs>
-            <Card typography="2" description="description of item 1"></Card>
+            <Card img={two} typography="2" description="description of item 2"></Card>
           </Grid>
           <Grid item xs>
-            <Card typography="3" description="description of item 1"></Card>
+            <Card img={three} typography="3" description="description of item 3"></Card>
           </Grid>
         </Grid>
         <Grid container spacing={2}>
           <Grid item xs>
-            <Card typography="4" description="description of item 4"></Card>
+            <Card img={four} typography="4" description="description of item 4"></Card>
           </Grid>
           <Grid item xs>
-            <Card typography="5" description="description of item 5"></Card>
+            <Card img={five} typography="5" description="description of item 5"></Card>
           </Grid>
           <Grid item xs>
-            <Card typography="6" description="description of item 6"></Card>
+            <Card img={six} typography="6" description="description of item 6"></Card>
           </Grid>
         </Grid>
       </div>
